8d5d448031c20be4c5b195409b9973176a0318e8
zghrib <zeineb@hexagone-ai.com>
2022-11-24 19:18:31 +0100

    env vars

--- api/__init__.py
+++ api/__init__.py
@@ -7,7 +7,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 dotenv_path = find_dotenv()
 # load up the entries as environment variables
 load_dotenv(dotenv_path)
-wandb_api = os.environ.get("wandb_api")
+wandb_api = os.environ.get("WANDB_API")
 
 config = configparser.ConfigParser()
 config.read('config.ini')
